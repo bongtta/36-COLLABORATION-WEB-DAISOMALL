40cd682e28d9711a1d18badfb9278bfd2a6e6abf
Kyoungho Eom <kheom0512@gmail.com>
2025-05-14 12:23:08 +0900

    chore: 의존성 업데이트 및 lockfile 갱신

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
@@ -17,6 +18,7 @@ export default defineConfig({
       },
     }
     ),
+    tsconfigPaths()
   ],
  },
 )
