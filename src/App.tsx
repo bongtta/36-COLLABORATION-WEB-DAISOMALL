10b325c547b78e8df3160c3e0b136a0b3b1cbf82
bongtta <kce0419@naver.com>
2025-05-16 03:14:22 +0900

    chore: 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
+import Header from '@components/Header/Header';
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
+        <Header />
       </ThemeProvider>
     </>
   );
