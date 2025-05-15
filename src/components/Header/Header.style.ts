d1cfe3086fe6a30776fd6457d33dd4dc5966f090
bongtta <kce0419@naver.com>
2025-05-16 02:26:29 +0900

    feat: 헤더 퍼블리싱

---
+++ src/components/Header/Header.style.ts
@@ -0,0 +1,36 @@
+import { css } from '@emotion/react';
+import theme from '@styles/theme';
+
+export const Wrapper = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1.6rem;
+  height: 4.8rem;
+`;
+
+export const LeftContainer = css`
+  display: flex;
+  align-items: center;
+  gap: 1.6rem;
+`;
+
+export const NavIcons = css`
+  margin: 1.2rem 0.5rem 1.2rem 0.6rem;
+  width: 2.4rem;
+`;
+
+export const Title = css`
+  width: 18.3rem;
+  ${theme.fonts['heading-16B']}
+  color: ${theme.colors.black};
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+export const RightContainer = css`
+  display: flex;
+  /* padding: 0 1.2rem; */
+  gap: 0.4rem;
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,39 @@
-const Header = () => {
-  return (
-    <>
-      <div>
-        <div></div>
-        <div></div>
+import { BackIcon, CartIcon, Logo, SearchIcon, TohomeIcon } from '@assets/svgs';
+import * as S from './Header.style';
+
+interface HeaderProps {
+  title?: string;
+  showLogo?: boolean;
+  showBackButton?: boolean;
+  showTitle?: boolean;
+  showSearchIcon?: boolean;
+  showHomeIcon?: boolean;
+  showCartIcon?: boolean;
+}
+
+const Header = ({
+  //   Todo: title,
+  showLogo = false,
+  showBackButton = true,
+  showTitle = true,
+  showSearchIcon = false,
+  showHomeIcon = false,
+  showCartIcon = true,
+}: HeaderProps) => (
+  <>
+    <div css={S.Wrapper}>
+      <div css={S.LeftContainer}>
+        {showLogo && <Logo width={73} />}
+        {showBackButton && <BackIcon width={24} />}
+        {showTitle && <p css={S.Title}>매장 상품 찾기</p>}
       </div>
-    </>
-  );
-};
+      <div css={S.RightContainer}>
+        {showSearchIcon && <SearchIcon css={S.NavIcons} />}
+        {showHomeIcon && <TohomeIcon css={S.NavIcons} />}
+        {showCartIcon && <CartIcon css={S.NavIcons} />}
+      </div>
+    </div>
+  </>
+);
 
 export default Header;
