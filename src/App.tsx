f1deefa03574daaae14e89397be1bfea5ddead78
bongtta <kce0419@naver.com>
2025-05-16 20:15:07 +0900

    chore: 테스트 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
-import HeaderNav from '@components/HeaderNav/HeaderNav';
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
-        <HeaderNav />
       </ThemeProvider>
     </>
   );
