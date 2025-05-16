8a91df6a513de5b177029ffad86d185dd11cbaa1
Dubabbi <thdms3160@naver.com>
2025-05-16 19:51:08 +0900

    chore: 테스트 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
-import SearchBar from '@components/SearchBar/SearchBar';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Global styles={GlobalStyle} />
-      <div style={{ padding: '2rem' }}>
-        <SearchBar />
-      </div>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <Global styles={GlobalStyle} />
+      </ThemeProvider>
+    </>
   );
 }
 
