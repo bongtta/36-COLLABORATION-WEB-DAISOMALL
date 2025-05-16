afa7023bb0631a662767530e6bbef7ad33466471
jisusin <sjsz0811@hanyang.ac.kr>
2025-05-17 02:15:05 +0900

    style: ViewMoreButton 컴포넌트 스타일 단위 및 코드 개선

--- src/components/buttons/viewMoreButton/ViewMoreButton.style.ts
+++ src/components/buttons/viewMoreButton/ViewMoreButton.style.ts
@@ -5,6 +5,7 @@ export const viewMoreContainer = css`
   width: 100%;
   display: flex;
   justify-content: center;
+  
   margin: 1.5rem 0;
 `;
 
@@ -12,29 +13,30 @@ export const buttonStyle = css`
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 0.4rem; // 4px (10px 기준)
-  width: 34.3rem; // 343px (10px 기준)
-  padding: 1.5rem 11.5rem; // 15px 115px (10px 기준)
+  gap: 0.4rem;
+  
+  width: 34.3rem;
+  padding: 1.5rem 11.5rem;
+  
   background-color: ${theme.colors.white};
-  border: 0.1rem solid ${theme.colors['gray-04']}; // 1px (10px 기준)
-  border-radius: 0.4rem; // 4px (10px 기준)
-  box-shadow: 0px 0.4rem 0.4rem 0px rgba(0, 0, 0, 0.25); // 4px (10px 기준)
+  border: 0.1rem solid ${theme.colors['gray-04']};
+  border-radius: 0.4rem;
+  box-shadow: 0px 0.4rem 0.4rem 0px rgba(0, 0, 0, 0.25);
+  
   cursor: pointer;
   box-sizing: border-box;
 `;
 
 export const textStyle = css`
   color: ${theme.colors['gray-01']};
-  font-family: Pretendard;
-  font-size: 1.4rem; // button 14B (10px 기준)
-  font-style: normal;
-  font-weight: 700;
-  line-height: 145%;
+  ${theme.fonts['button-14B']}
+  
   white-space: nowrap;
 `;
 
 export const iconStyle = css`
   color: ${theme.colors['gray-02']};
+  
   min-width: 0.75rem;
   min-height: 0.75rem;
 `;
@@ -42,6 +44,7 @@ export const iconStyle = css`
 export const contentStyle = css`
   width: 100%;
   padding-top: 1rem;
+  
   color: ${theme.colors['gray-01']};
   ${theme.fonts['body-14R']}
 `;
