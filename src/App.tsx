7fbe94fe1fbb2fe7bd4524664b697eebd1956e90
bongtta <kce0419@naver.com>
2025-05-17 13:07:58 +0900

    feat: 반응형 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
+import BottomNav from '@components/BottomNav/BottomNav';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
       </ThemeProvider>
+      <BottomNav />
     </>
   );
 }
