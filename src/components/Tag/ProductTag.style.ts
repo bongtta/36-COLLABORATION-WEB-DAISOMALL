1d35b029ad4dee20c8a873996ab4083637581d54
Kyoungho Eom <kheom0512@gmail.com>
2025-05-17 14:26:38 +0900

    feat: SVG 아이콘 및 태그 컴포넌트 추가

---
+++ src/components/Tag/ProductTag.style.ts
@@ -0,0 +1,10 @@
+import { css } from '@emotion/react';
+import theme from '@styles/theme';
+
+export const Wrapper = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1.6rem;
+  height: 4.8rem;
+`;
---
+++ src/components/Tag/ProductTag.tsx
@@ -0,0 +1,12 @@
+import { StarIconWhite } from '@assets/svgs';
+import * as S from './ProductTag.style';
+
+const ProductTag = () => {
+  return (
+    <S.Wrapper>
+      <StarIconWhite />
+    </S.Wrapper>
+  );
+};
+
+export default ProductTag;
