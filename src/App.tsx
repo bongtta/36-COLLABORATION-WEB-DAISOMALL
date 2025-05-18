ed9c72b67f73169859000610489cec2dede9d229
Dubabbi <thdms3160@naver.com>
2025-05-18 14:12:02 +0900

    feat: 영업 상태 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
+import StoreStatus from '@components/LocationCard/StoreStatus/StoreStatus';
 
 function App() {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Global styles={GlobalStyle} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <Global styles={GlobalStyle} />
+      <div
+        style={{
+          padding: '2rem',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+        }}
+      >
+        {/* 영업 중 */}
+        <StoreStatus isOpen={true} />
+        {/* 영업 전 */}
+        <StoreStatus isOpen={false} />
+      </div>
+    </ThemeProvider>
   );
 }
 
