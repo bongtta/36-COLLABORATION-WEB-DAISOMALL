7e5831148f1436abd390b75a90f88a337b96e4e2
Dubabbi <thdms3160@naver.com>
2025-05-18 10:45:18 +0900

    chore: 테스트 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,14 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
-import ProductCardRanking from '@components/ProductCard/ProductCardRanking/ProductCardRanking';
-import ProductCardVertical from '@components/ProductCard/ProductCardVertical/ProductCardVertical';
-import TodayCard from '@components/ProductCard/TodayCard/TodayCard';
-import Img from '@assets/image.png';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Global styles={GlobalStyle} />
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: 'full',
-          gap: '2rem',
-          padding: '2rem',
-        }}
-      >
-        <ProductCardRanking
-          name="랭킹용 앰플"
-          price="3,000"
-          imageUrl={Img}
-          showCartIcon={true}
-          code="123456"
-          withOutline={true}
-        />
-        <ProductCardRanking
-          rank={1}
-          name="랭킹용 앰플"
-          price="3,000"
-          imageUrl={Img}
-          showCartIcon={false}
-          tags={['박스배송', '택배전용']}
-        />
-        <div
-          style={{
-            display: 'flex',
-            gap: '2rem',
-          }}
-        >
-          <ProductCardVertical
-            size="96"
-            name="수직형 앰플"
-            price="3,000"
-            imageUrl={Img}
-            quantityText="1개"
-            tags={['박스배송', '택배전용']}
-            isBoxDelivery={true}
-          />
-          <ProductCardVertical
-            size="96"
-            name="수직형 앰플"
-            price="3,000"
-            imageUrl={Img}
-            tags={['SNS']}
-            isSnsHot={true}
-          />
-        </div>
-        <TodayCard
-          imageUrl={Img}
-          profileImage={Img}
-          username="왓정"
-          title="겨울철 가성비피부관리 화장품후기 (VT리들샷, 린제이모델링팩 등)"
-          views={37120}
-        />
-      </div>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <Global styles={GlobalStyle} />
+      </ThemeProvider>
+    </>
   );
 }
 
