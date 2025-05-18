8d3d4295e7ec7299bf7fc0db6c2c7b59a7a13a93
Dubabbi <thdms3160@naver.com>
2025-05-18 11:39:24 +0900

    chore: 테스트 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
-import StoreSearchBar from './components/StoreSearchBar/StoreSearchBar';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Global styles={GlobalStyle} />
-      <div style={{ padding: '2rem' }}>
-        <StoreSearchBar />
-      </div>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <Global styles={GlobalStyle} />
+      </ThemeProvider>
+    </>
   );
 }
 
