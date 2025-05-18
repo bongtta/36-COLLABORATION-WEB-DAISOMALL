2dadf5068ee0e1e3cd7171855fa36a56a76517b5
Dubabbi <thdms3160@naver.com>
2025-05-18 22:15:05 +0900

    chore: 테스트 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
-import QuickMenuBar from '@components/QuickMenuBar/QuickMenuBar';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Global styles={GlobalStyle} />
-      <div style={{ padding: '2rem' }}>
-        <QuickMenuBar
-          onStoreSearchClick={() => console.log('매장재고 클릭')}
-          onHolidayArrivalClick={() => console.log('휴일도착 클릭')}
-        />
-      </div>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <Global styles={GlobalStyle} />
+      </ThemeProvider>
+    </>
   );
 }
 
