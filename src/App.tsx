06b9f092060f34adf823f5908bda618856792d9d
Dubabbi <thdms3160@naver.com>
2025-05-18 11:27:42 +0900

    feat: 매장 상품 찾기 검색 바 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
+import StoreSearchBar from '@components/StoreSearch/StoreSearchBar';
 
 function App() {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Global styles={GlobalStyle} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <Global styles={GlobalStyle} />
+      <div style={{ padding: '2rem' }}>
+        <StoreSearchBar />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,1 +1,3 @@
 export { default as React } from './React';
+export { default as SearchIcon } from './SearchIcon';
+export { default as LocationIcon } from './LocationIcon';
