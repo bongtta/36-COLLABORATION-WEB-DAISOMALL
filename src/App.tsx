ae39f4ef01ae055b3ca45fc135eca2e791d5c34a
Kyoungho Eom <kheom0512@gmail.com>
2025-05-19 23:35:35 +0900

    feat: 필터 탭 및 SVG 아이콘 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
+import FilterTabs from './components/tabs/FilterTabs';
+
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
+        <FilterTabs />
       </ThemeProvider>
     </>
   );
