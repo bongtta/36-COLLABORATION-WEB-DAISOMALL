b4e5cc860dac85073f4a699831eb844c1f8e8652
Kyoungho Eom <kheom0512@gmail.com>
2025-05-21 01:44:54 +0900

    feat: 라우터 및 테스트 페이지 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Global, ThemeProvider} from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 import { BrowserRouter } from 'react-router-dom';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
+import Router from '@routes/Router';
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
+        <Router />
       </ThemeProvider>
     </BrowserRouter>
   );
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>홈 페이지</h1>
+      <p>메인 페이지입니다.</p>
+    </div>
+  );
+};
+
+export default HomePage; 
