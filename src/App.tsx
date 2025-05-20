eaa5b5819647b7fad2b5c8346448e2e507cda337
Kyoungho Eom <kheom0512@gmail.com>
2025-05-21 01:39:54 +0900

    feat: BrowserRouter 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { Global, ThemeProvider, css } from '@emotion/react';
+import { Global, ThemeProvider} from '@emotion/react';
+import { BrowserRouter } from 'react-router-dom';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
       </ThemeProvider>
-    </>
+    </BrowserRouter>
   );
 }
 
