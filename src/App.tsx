974ee860c24a0c4a191b3354c2d30b8b525a0cf1
Kyoungho Eom <kheom0512@gmail.com>
2025-05-20 10:09:09 +0900

    feat: 배너 및 인디케이터 UI 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,39 @@
 /** @jsxImportSource @emotion/react */
-import { Global, ThemeProvider, css } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
-import OrderIndicator from './components/OrderIndicator/OrderIndicator';
-import bannerImage from '../public/banner.png';
+import ImageCarousel from './components/ImageCarousel/ImageCarousel';
 
-const bannerContainerStyle = css`
-  position: relative;
-  width: 100%;
-`;
+// 배너 이미지 import
+import banner1 from '../public/banner1.png';
+import banner2 from '../public/banner2.png';
+import banner3 from '../public/banner3.png';
+import banner4 from '../public/banner4.png';
+import banner5 from '../public/banner5.png';
+import banner6 from '../public/banner6.png';
 
-const bannerImageStyle = css`
-  width: 100%;
-  display: block;
-`;
-
-const indicatorPositionStyle = css`
-  position: absolute;
-  bottom: 1.1rem;
-  right: 1.6rem;
-`;
+// 배너 이미지 배열
+const bannerImages = [
+  banner1,
+  banner2,
+  banner3,
+  banner4,
+  banner5,
+  banner6
+];
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
-        <div css={bannerContainerStyle}>
-          <img src={bannerImage} alt="Banner" css={bannerImageStyle} />
-          <div css={indicatorPositionStyle}>
-            <OrderIndicator currentIndex={1} totalItems={5} />
-          </div>
-        </div>
+
+        <ImageCarousel 
+          images={bannerImages} 
+          bottomPadding="1.1rem"
+          autoSlideInterval={3000} // 3초마다 슬라이드 전환 (원하는 값으로 조정 가능)
+        />
+
       </ThemeProvider>
     </>
   );
