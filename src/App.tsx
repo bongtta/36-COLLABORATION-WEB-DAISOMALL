8152f24237d678754b4c798a9ce259ed8591142b
jisusin <sjsz0811@hanyang.ac.kr>
2025-05-21 01:18:30 +0900

    test: 홈 캐러셀 컴포넌트 테스트 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Global, ThemeProvider, css } from '@emotion/react';
+/** @jsxImportSource @emotion/react */
+import { ThemeProvider } from '@emotion/react';
+import { Global } from '@emotion/react';
 import theme from '@styles/theme';
-import GlobalStyle from './styles/global';
-import ProductTag from './components/Tag/ProductTag';
-import { productTags } from './components/Tag/tagData';
+import GlobalStyle from '@styles/global';
+import HomeCarousel from './components/homeCarousel/HomeCarousel';
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
+        <div css={{ maxWidth: '37.5rem', margin: '0 auto' }}>
+          <HomeCarousel />
+        </div>
       </ThemeProvider>
     </>
   );
