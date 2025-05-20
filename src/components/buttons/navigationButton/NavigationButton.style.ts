1ff620ff57dfe75d99e22e83b19653c24742e151
jisusin <sjsz0811@hanyang.ac.kr>
2025-05-20 13:42:36 +0900

    style: NavigationButton 디자인 수정

--- src/components/buttons/navigationButton/NavigationButton.style.ts
+++ src/components/buttons/navigationButton/NavigationButton.style.ts
@@ -3,52 +3,41 @@ import theme from '@styles/theme';
 
 export const buttonContainer = css`
   width: 100%;
-  
   display: flex;
   justify-content: center;
-  
-  margin: 1rem 0;
 `;
 
 export const buttonStyle = (variant: 'white' | 'gray') => css`
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 0.8rem;
+  gap: ${variant === 'white' ? '0.4rem' : '0.8rem'};
   
-  width: 34.3rem;
+  width: 100%;
+  max-width: 34.3rem;
+  height: 4.8rem;
   padding: 1.5rem 11.5rem;
   
   background-color: ${variant === 'white' ? theme.colors.white : theme.colors['gray-06']};
-  border: ${variant === 'white' ? `0.1rem solid ${theme.colors['gray-04']}` : 'none'};
-  border-radius: 0.4rem;
+  border: ${variant === 'white' ? `1px solid ${theme.colors['gray-04']}` : 'none'};
+  border-radius: 4px;
   
   cursor: pointer;
-  box-sizing: border-box;
 `;
 
 export const textStyle = css`
   color: ${theme.colors['gray-01']};
-  
   ${theme.fonts['button-14B']}
   white-space: nowrap;
+  display: flex;
+  align-items: center;
 `;
 
 export const countStyle = css`
   color: ${theme.colors.primary};
-  
   ${theme.fonts['button-14B']}
-`;
-
-export const iconStyle = css`
-  width: 0.4rem;
-  height: 0.8rem;
-  
-  flex-shrink: 0;
-  
-  color: ${theme.colors['gray-02']};
-  stroke-width: 0.1rem;
-  stroke: ${theme.colors['gray-02']};
+  display: flex;
+  align-items: center;
 `;
 
 export const iconContainer = css`
@@ -58,4 +47,9 @@ export const iconContainer = css`
   
   width: 1.6rem;
   height: 1.6rem;
+  flex-shrink: 0;
+`;
+
+export const iconStyle = css`
+  flex-shrink: 0;
 `;
