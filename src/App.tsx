119975108f1e9e14fa36e5e3efb383d1f072527f
bongtta <kce0419@naver.com>
2025-05-21 18:01:29 +0900

    setting: tanstack-query 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,31 @@ import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import pageRoutes from '@routes/pageRoutes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
   const router = createBrowserRouter([...pageRoutes]);
 
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
   return (
-    <ThemeProvider theme={theme}>
-      <Global styles={GlobalStyle} />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Global styles={GlobalStyle} />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+      <div style={{ fontSize: '16px' }}>
+        <ReactQueryDevtools />
+      </div>
+    </QueryClientProvider>
   );
 }
 
