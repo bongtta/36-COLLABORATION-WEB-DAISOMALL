4e723dc9b5b8068a934540a585a194d582281f08
jisusin <sjsz0811@hanyang.ac.kr>
2025-05-22 21:49:34 +0900

    feat: 매장 상품 찾기 페이지에 지금 많이 찾는 상품 콘텐츠 섹션 추가

--- src/pages/StoreSearchPage/StoreSearchPage.tsx
+++ src/pages/StoreSearchPage/StoreSearchPage.tsx
@@ -1,37 +1,25 @@
+import React from 'react';
 import Header from '@components/Header/Header';
 import Divider from '@components/common/divider/Divider';
-import SearchBar from '@components/SearchBar/SearchBar';
 import BottomNav from '@components/BottomNav/BottomNav';
 import ChatbotButton from '@components/buttons/floatingButtons/ChatbotButton';
 import FloatingUpButton from '@components/buttons/floatingButtons/FloatingUpButton';
-import * as S from './StoreSearchPage.style';
+import PopularProductsSection from './PopularProductsSection';
+import { pageLayoutStyle, contentAreaStyle } from './StoreSearchPage.style';
 
-const StoreSearchPage = () => {
-  return (
-    <>
-      {/* 헤더 */}
-      <Header showBackButton showTitle title="매장 상품 찾기" showCartIcon />
-      <Divider />
-
-      {/* 메인 컨텐츠 */}
-      <div css={S.ContentContainer}>
-        {/* 검색바 */}
-        <SearchBar placeholder="상품명, 품번, 브랜드" />
-
-        {/* 결과 섹션 */}
-        <div css={S.ResultSection}>
-          <p>검색어를 입력해주세요</p>
-        </div>
-      </div>
-
-      {/* 플로팅 버튼들 */}
-      <ChatbotButton />
-      <FloatingUpButton />
-
-      {/* 바텀 네비게이션 */}
-      <BottomNav />
-    </>
-  );
-};
+const StoreSearchPage = () => (
+  <div css={pageLayoutStyle}>
+    <Header showBackButton showTitle title="매장 상품 찾기" showCartIcon />
+    <Divider />
+    
+    <div css={contentAreaStyle}>
+      <PopularProductsSection />
+    </div>
+    
+    <ChatbotButton />
+    <FloatingUpButton />
+    <BottomNav />
+  </div>
+);
 
 export default StoreSearchPage;
