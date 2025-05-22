89fdf60dcbdf80bf58d2073346479f124de73e80
jisusin <sjsz0811@hanyang.ac.kr>
2025-05-22 21:45:41 +0900

    feat: 헤더 이전(Back) 버튼에 뒤로가기 기능 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import { BackIcon, CartIcon, Logo, SearchIcon, TohomeIcon } from '@assets/svgs';
 import * as S from './Header.style';
+import { useNavigate } from 'react-router-dom';
 
 interface HeaderProps {
   title?: string;
@@ -19,21 +20,31 @@ const Header = ({
   showSearchIcon = false,
   showHomeIcon = false,
   showCartIcon = false,
-}: HeaderProps) => (
-  <>
-    <div css={S.Wrapper}>
-      <div css={S.LeftContainer}>
-        {showLogo && <Logo width={73} />}
-        {showBackButton && <BackIcon width={24} />}
-        {showTitle && <p css={S.Title}>{title}</p>}
+}: HeaderProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div css={S.Wrapper}>
+        <div css={S.LeftContainer}>
+          {showLogo && <Logo width={73} />}
+          {showBackButton && (
+            <BackIcon
+              width={24}
+              onClick={() => navigate(-1)}
+              style={{ cursor: 'pointer' }}
+            />
+          )}
+          {showTitle && <p css={S.Title}>{title}</p>}
+        </div>
+        <div css={S.RightContainer}>
+          {showSearchIcon && <SearchIcon css={S.NavIcons} />}
+          {showHomeIcon && <TohomeIcon css={S.NavIcons} />}
+          {showCartIcon && <CartIcon css={S.NavIcons} />}
+        </div>
       </div>
-      <div css={S.RightContainer}>
-        {showSearchIcon && <SearchIcon css={S.NavIcons} />}
-        {showHomeIcon && <TohomeIcon css={S.NavIcons} />}
-        {showCartIcon && <CartIcon css={S.NavIcons} />}
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Header;
