ecb10842059e7a3131ec4ee0fd6e58a91af7154f
Kyoungho Eom <kheom0512@gmail.com>
2025-05-22 14:40:39 +0900

    feat: 픽업 방법 리스트를 컴포넌트화하여 재사용성 개선

--- src/pages/ProductDetailPage/PickupInfo/MethodListSection.tsx
+++ src/pages/ProductDetailPage/PickupInfo/MethodListSection.tsx
@@ -4,54 +4,73 @@ import DeliveryIconCycle from '@assets/svgs/DeliveryIconCycle';
 import StoreIcon from '@assets/svgs/StoreIcon';
 import BetaMark from '@assets/svgs/BetaMark';
 import BoxIcon from '@assets/svgs/BoxIcon';
+import type { ReactNode } from 'react';
+
+interface MethodItemProps {
+  icon: ReactNode;
+  name: string;
+  date: string;
+  isStore?: boolean;
+  isBeta?: boolean;
+}
+
+const MethodItem = ({ icon, name, date, isStore = false, isBeta = false }: MethodItemProps) => {
+  return (
+    <li css={[S.MethodItem, isStore && S.StoreMethodItem]}>
+      {isBeta && <BetaMark css={S.BetaIconStyle} />}
+      <div css={S.MethodItemContent}>
+        <div css={S.IconWrapper}>
+          {icon}
+        </div>
+        <span css={S.MethodName} className={isStore ? "method-name" : ""}>
+          {name}
+        </span>
+        <span css={S.MethodDate} className={isStore ? "method-date" : ""}>
+          {date}
+        </span>
+      </div>
+    </li>
+  );
+};
 
 const MethodListSection = () => { 
+  const methodItems: MethodItemProps[] = [
+    {
+      icon: <DeliveryIconTruck />,
+      name: '택배배송',
+      date: '4/29(화)\n도착예정',
+    },
+    {
+      icon: <StoreIcon />,
+      name: '매장픽업',
+      date: '4/28(월)\n픽업가능',
+      isStore: true,
+    },
+    {
+      icon: <DeliveryIconCycle />,
+      name: '오늘배송',
+      date: '4/28(월)\n도착예정',
+      isBeta: true,
+    },
+    {
+      icon: <BoxIcon />,
+      name: '대량주문',
+      date: '7일 이내\n도착예정',
+    },
+  ];
+
   return (
     <ul css={S.MethodList}>
-      <li css={S.MethodItem}>
-        <div css={S.MethodItemContent}>
-          <div css={S.IconWrapper}>
-            <DeliveryIconTruck />
-          </div>
-          <span css={S.MethodName}>택배배송</span>
-          <span css={S.MethodDate}>4/29(화)<br/>도착예정</span>
-        </div>
-      </li>
-
-      
-      <li css={[S.MethodItem, S.StoreMethodItem]}>
-        <div css={S.MethodItemContent}>
-          <div css={S.IconWrapper}>
-            <StoreIcon />
-          </div>
-          <span css={S.MethodName} className="method-name">매장픽업</span>
-          <span css={S.MethodDate} className="method-date">4/28(월)<br/>픽업가능</span>
-        </div>
-      </li>
-
-      <li css={S.MethodItem}>
-        <BetaMark css={S.BetaIconStyle} />
-        <div css={S.MethodItemContent}>
-          <div css={S.IconWrapper}>
-            <DeliveryIconCycle />
-          </div>
-          <span css={S.MethodName}>오늘배송</span>
-          <span css={S.MethodDate}>4/28(월)<br/>도착예정</span>
-        </div>
-      </li>
-
-
-      <li css={S.MethodItem}>
-        <div css={S.MethodItemContent}>
-          <div css={S.IconWrapper}>
-            <BoxIcon />
-          </div>
-          <span css={S.MethodName}>대량주문</span>
-          <span css={S.MethodDate}>7일 이내<br/>도착예정</span>
-        </div>
-      </li>
-
-
+      {methodItems.map((item, index) => (
+        <MethodItem
+          key={index}
+          icon={item.icon}
+          name={item.name}
+          date={item.date}
+          isStore={item.isStore}
+          isBeta={item.isBeta}
+        />
+      ))}
     </ul>
   );
 };
