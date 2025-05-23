74af4dd57d0eae2d9d4a9369b89e700c90341d87
bongtta <kce0419@naver.com>
2025-05-23 13:59:07 +0000

    fix: 절대경로 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
@@ -17,4 +17,18 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@apis': path.resolve(__dirname, './src/apis'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@app-types': path.resolve(__dirname, './src/types'),
+    }
+  }
 });
