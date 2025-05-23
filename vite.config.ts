0875461c0f9e9a4f5f0a9ab157d7cf8b849b22b5
bongtta <kce0419@naver.com>
2025-05-23 13:44:31 +0000

    chore: 절대경로 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,19 +11,10 @@ export default defineConfig({
       jsxImportSource: '@emotion/react',
       babel: { plugins: ['@emotion/babel-plugin'] },
     }),
-    svgr({ svgrOptions: { icon: true } }),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
   ],
-  resolve: {
-    alias: {
-      '@styles': path.resolve(__dirname, 'src/styles'),
-      '@routes': path.resolve(__dirname, 'src/routes'),
-      '@apis': path.resolve(__dirname, 'src/apis'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@constants': path.resolve(__dirname, 'src/constants'),
-      '@app-types': path.resolve(__dirname, 'src/types'),
-    },
-  },
 });
