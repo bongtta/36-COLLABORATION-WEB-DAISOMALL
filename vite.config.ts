26f38235f4e2ddb08135f229fb1c1c6eb07d929c
bongtta <kce0419@naver.com>
2025-05-23 13:35:18 +0000

    fix: alias 경로 추가

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,14 @@ export default defineConfig({
   resolve: {
     alias: {
       '@styles': path.resolve(__dirname, 'src/styles'),
+      '@routes': path.resolve(__dirname, 'src/routes'),
+      '@apis': path.resolve(__dirname, 'src/apis'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@app-types': path.resolve(__dirname, 'src/types'),
     },
   },
 });
